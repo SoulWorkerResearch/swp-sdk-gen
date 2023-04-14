09e79a27ebf6afcab776c660f8e9dff8d7a3e05a
sawich <sawich@angeldev.org>
2023-04-14 06:37:47 +0300

    ✨ feat(swp-sdk-gen): add precompiled headers support
    Add precompiled headers support to the swp-sdk-gen project.

---
+++ swp-sdk-gen/headers/pch.hpp
@@ -0,0 +1,29 @@
+// pch.h: This is a precompiled header file.
+// Files listed below are compiled only once, improving build performance for future builds.
+// This also affects IntelliSense performance, including code completion and many code browsing features.
+// However, files listed here are ALL re-compiled if any one of them is updated between builds.
+// Do not add files here that you will be updating frequently as this negates the performance advantage.
+
+#ifndef PCH_H
+#define PCH_H
+
+#define SPDLOG_USE_STD_FORMAT
+
+// add headers that you want to pre-compile here
+#include "framework.hpp"
+
+#include <filesystem>
+#include <ranges>
+#include <span>
+#include <string>
+#include <iostream>
+#include <cassert>
+#include <execution>
+#include <spanstream>
+#include <fstream>
+#include <vector>
+#include <regex>
+
+#include <spdlog/spdlog.h>
+
+#endif //PCH_H
